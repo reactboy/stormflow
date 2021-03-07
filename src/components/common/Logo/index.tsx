407a570de05ea58214f5c6c587e4ed537a3fae88
Jun Aida <junceliz@gmail.com>
2021-03-07 10:02:41 +0900

    add : implement size option for logo

--- src/components/common/Logo/index.tsx
+++ src/components/common/Logo/index.tsx
@@ -1,11 +1,44 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex, Text , FlexProps} from '@chakra-ui/react';
 import { LogoIcon } from '@components/common/Icon';
 
-export const Logo = (props) => (
-  <Flex alignItems="center" {...props}>
-    <LogoIcon boxSize={12} />
-    <Text fontFamily="IBM Plex Mono" fontStyle="italic" fontWeight="medium" lineHeight={1} ml="4px" fontSize="48px">
-      Stormflow
-    </Text>
-  </Flex>
-);
+const logoSize = {
+  sm: {
+    fontSize: '16px',
+    boxSize: 4,
+    ml: '2px',
+  },
+  md: {
+    fontSize: '32px',
+    boxSize: 8,
+    ml: '4px',
+  },
+  lg: {
+    fontSize: '48px',
+    boxSize: 12,
+    ml: '4px',
+  },
+};
+
+type LogoProps = {
+  size?: keyof typeof logoSize;
+  withIcon?: boolean;
+};
+
+export const Logo: React.FC<LogoProps & FlexProps> = (props) => {
+  const { withIcon = true, size = 'lg', ...restProps } = props;
+  return (
+    <Flex alignItems="center" {...restProps}>
+      {withIcon && <LogoIcon boxSize={logoSize[size].boxSize} />}
+      <Text
+        fontFamily="IBM Plex Mono"
+        fontStyle="italic"
+        fontWeight="medium"
+        lineHeight={1}
+        ml={withIcon ? logoSize[size].ml : 0}
+        fontSize={logoSize[size].fontSize}
+      >
+        Stormflow
+      </Text>
+    </Flex>
+  );
+};
