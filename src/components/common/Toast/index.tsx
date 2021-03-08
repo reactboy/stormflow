dda66e95d40e9db20b59a4f8a7f2791da6444228
Jun Aida <junceliz@gmail.com>
2021-03-08 23:04:11 +0900

    add : adjust toast style

--- src/components/common/Toast/index.tsx
+++ src/components/common/Toast/index.tsx
@@ -1,15 +1,40 @@
 import { Box } from '@chakra-ui/react';
 
-type ToastStatus = 'success' | 'fatal';
+const toastStatus = {
+  default: {
+    borderColor: 'gray.200',
+    bg: 'white',
+    color: 'gray.900',
+  },
+  //TODO status毎に色を変える
+  success: { borderColor: 'gray.200', bg: 'white', color: 'gray.900' },
+  fatal: { borderColor: 'gray.200', bg: 'white', color: 'gray.900' },
+};
+
 type ToastProps = {
-  status?: ToastStatus;
+  status?: keyof typeof toastStatus;
   children: string;
 };
 
 export const Toast: React.FC<ToastProps> = (props) => {
-  const { status = 'success', children } = props;
+  const { status = 'default', children } = props;
 
-  //TODO statusで見た目を変える
-
-  return <Box>{children}</Box>;
+  return (
+    //NOTE 真ん中に表示させるためにネストする
+    <Box textAlign="center">
+      <Box
+        display="inline-block"
+        border="1px"
+        borderColor={toastStatus[status].borderColor}
+        borderRadius="8px"
+        bg={toastStatus[status].bg}
+        color={toastStatus[status].color}
+        fontWeight="bold"
+        py="8px"
+        px="16px"
+      >
+        {children}
+      </Box>
+    </Box>
+  );
 };
