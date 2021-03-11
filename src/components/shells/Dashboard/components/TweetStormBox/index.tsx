164d7899d08c17a181a750fa5aa5a1aeff201bd1
Jun Aida <junceliz@gmail.com>
2021-03-11 23:20:04 +0900

    add : add hover animation for storm

--- src/components/shells/Dashboard/components/TweetStormBox/index.tsx
+++ src/components/shells/Dashboard/components/TweetStormBox/index.tsx
@@ -20,11 +20,15 @@ export const TweetStormBox: React.FC<TweetStormBoxProps> = (props) => {
       borderY="1px"
       borderColor="gray.200"
       _notFirst={{ borderTop: 'none' }}
+      role="group"
+      overflowX="hidden"
     >
       <Text textAlign="right" fontSize="8px">
         {formatDateString(createdAt)}
       </Text>
-      <Text fontSize="16px">{text}</Text>
+      <Text transition="all ease .4s" _groupHover={{ fontWeight: 'bold' }} fontSize="16px">
+        {text}
+      </Text>
       {!!includes.length &&
         includes.map((tweet) => <TweetStorm level={0 + 1} key={tweet.id} tweet={tweet} />)}
       <Flex pt="4px">
@@ -37,6 +41,9 @@ export const TweetStormBox: React.FC<TweetStormBoxProps> = (props) => {
           onClick={onAddStorm(idStr)}
           aria-label="add storm"
           icon={<LogoIcon boxSize={5} />}
+          transform="translateX(100%)"
+          opacity="0"
+          _groupHover={{ transform: 'translateX(0)', opacity: 1 }}
         />
       </Flex>
     </Box>
